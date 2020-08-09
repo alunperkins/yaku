907a3f17638bd50a4b2eb78354048f778f5feca9
Alun Perkins <alun.perkins1@gmail.com>
2020-08-09 10:04:33 +0100

    add licence

--- indexcreator/src/main/java/com/alun/indexcreator/models/LoanSourceAttrs.kt
+++ indexcreator/src/main/java/com/alun/indexcreator/models/LoanSourceAttrs.kt
@@ -1,3 +1,22 @@
+/*
+    Yaku offline browser of Japanese dictionaries
+    Copyright (C) 2020 Alun Perkins
+
+    This file is part of Yaku.
+
+    Yaku is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Yaku is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Yaku.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.alun.indexcreator.models
 
 enum class LoanSourceAttrs(val qName: String) {
